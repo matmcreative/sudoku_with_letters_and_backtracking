275d1f03019bd5ade8d93d4bb8d2667a23f101bc
Lisa Reed Preston <lisareedpreston@Lisas-MacBook-Pro.local>
2020-09-08 15:56:08 -0500

    first functions

---
+++ sudoku.py
@@ -0,0 +1,35 @@
+board = [
+    [0,0,0,2,6,0,7,0,1],
+    [6,8,0,0,7,0,0,9,0],
+    [1,9,0,0,0,4,5,0,0],
+    [8,2,0,1,0,0,0,4,0],
+    [0,0,4,1,0,0,9,0,0],
+    [0,5,0,0,0,3,0,2,8],
+    [0,0,9,3,0,0,0,7,4],
+    [0,4,0,6,0,2,9,0,0],
+    [7,0,3,0,1,8,0,0,0]
+]
+
+
+def print_board(brd):
+    for i in range(len(brd)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - ")
+
+        for j in range(len(brd[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(brd[i][j])
+            else:
+                print(str(brd[i][j]) + " ", end="")
+# print_board(board)
+
+def find_empty(brd):
+    for i in range(len(brd)):
+        for j in range(len(brd[0])):
+            if brd[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
