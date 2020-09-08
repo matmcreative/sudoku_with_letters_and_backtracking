521c449f453a47fe103725284606c5f4a938912d
Lisa Reed Preston <lisareedpreston@Lisas-MacBook-Pro.local>
2020-09-08 16:26:31 -0500

    solve and print board

--- sudoku.py
+++ sudoku.py
@@ -10,21 +10,8 @@ board = [
     [7,0,3,0,1,8,0,0,0]
 ]
 
-def valid(brd, num, pos):
-
-    #check row
-    for i in range(len(brd[0])):
-        if brd[pos[0][i]] == num and pos[1] != i:
-            return False
-    
-    #check column
-    for i in range(len(brd)):
-        if brd[[i], pos[1]] == num and pos[0] != i:
-            return False
-
 def solve(brd):
-
-    find = find_empty(brd):
+    find = find_empty(brd)
     if not find:
         return True
     else:
@@ -36,26 +23,39 @@ def solve(brd):
 
             if solve(brd):
                 return True
-            
+
             brd[row][col] = 0
 
     return False
 
-    #Check box
+
+def valid(brd, num, pos):
+    # Check row
+    for i in range(len(brd[0])):
+        if brd[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(brd)):
+        if brd[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
     for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x*3, box_x*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
             if brd[i][j] == num and (i,j) != pos:
                 return False
+
     return True
 
 
 def print_board(brd):
     for i in range(len(brd)):
         if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - ")
+            print("- - - - - - - - - - - - - ")
 
         for j in range(len(brd[0])):
             if j % 3 == 0 and j != 0:
@@ -65,7 +65,7 @@ def print_board(brd):
                 print(brd[i][j])
             else:
                 print(str(brd[i][j]) + " ", end="")
-# print_board(board)
+
 
 def find_empty(brd):
     for i in range(len(brd)):
@@ -74,3 +74,8 @@ def find_empty(brd):
                 return (i, j)  # row, col
 
     return None
+
+print_board(board)
+solve(board)
+print("___________________")
+print_board(board)
