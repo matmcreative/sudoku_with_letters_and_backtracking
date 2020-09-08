96d67beb79895389a47ac1d632d3025c3a43c371
Lisa Reed Preston <lisareedpreston@Lisas-MacBook-Pro.local>
2020-09-08 16:07:09 -0500

    valid function

--- sudoku.py
+++ sudoku.py
@@ -10,6 +10,27 @@ board = [
     [7,0,3,0,1,8,0,0,0]
 ]
 
+def valid(brd, num, pos):
+
+    #check row
+    for i in range(len(brd[0])):
+        if brd[pos[0][i]] == num and pos[1] != i:
+            return False
+    
+    #check column
+    for i in range(len(brd)):
+        if brd[[i], pos[1]] == num and pos[0] != i:
+            return False
+
+    #Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3)
+        for j in range(box_x*3, box_x*3 + 3)
+            if brd[i][j] == num and (i,j) != pos:
+                return False
+    return True
 
 def print_board(brd):
     for i in range(len(brd)):
